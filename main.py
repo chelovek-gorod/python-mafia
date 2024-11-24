64993b2ab6383e4de9a360ad4f567e4138494fbd
Marsel <47145234+chelovek-gorod@users.noreply.github.com>
2024-11-24 18:54:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from telebot import TeleBot
 import db
 from time import sleep
-from random import choice
 
-players = []
+
 game = False
-TOKEN = "7057302012:AAELgvrojjWbbglPEA1aylrHIUHSRKYYoEM"
+TOKEN = "TOKEN from BotFather"
 bot = TeleBot(TOKEN)
 night = False
+
+
 
 
 def get_killed(night):
@@ -18,37 +19,13 @@ def get_killed(night):
     return f'Мафия убила: {username_killed}'
 
 
-def autoplay_citizen(message):
-    players_roles = db.get_players_roles()
-    for player_id, _ in players_roles:
-        usernames = db.get_all_alive()
-        name = f'robot{player_id}'
-        if player_id < 5 and name in usernames:
-            usernames.remove(name)
-            vote_username = choice(usernames)
-            db.vote('citizen_vote', vote_username, player_id)
-            bot.send_message(
-                message.chat.id, f'{name} проголосовал против {vote_username}')
-            sleep(0.5)
-
-
-def autoplay_mafia():
-    players_roles = db.get_players_roles()
-    for player_id, role in players_roles:
-        usernames = db.get_all_alive()
-        name = f'robot{player_id}'
-        if player_id < 5 and name in usernames and role == 'mafia':
-            usernames.remove(name)
-            vote_username = choice(usernames)
-            db.vote('mafia_vote', vote_username, player_id)
-            
 
 
 def game_loop(message):
     global night, game
     bot.send_message(
         message.chat.id, "Добро пожаловать в игру! Вам дается 1 минута, чтобы познакомиться")
-    sleep(10)
+    sleep(60)
     while True:
         msg = get_killed(night)
         bot.send_message(message.chat.id, msg)
@@ -69,8 +46,9 @@ def game_loop(message):
         alive = db.get_all_alive()
         alive = '\n'.join(alive)
         bot.send_message(message.chat.id, text=f'В игре:\n{alive}')
-        sleep(10)
-        autoplay_mafia() if night else autoplay_citizen(message)
+        sleep(60)
+
+
 
 
 @bot.message_handler(func=lambda m: m.text.lower() == 'готов играть' and m.chat.type == 'private')
@@ -79,6 +57,17 @@ def send_text(message):
     bot.send_message(message.from_user.id, 'Вы добавлены в игру')
     db.insert_player(message.from_user.id,
                      username=message.from_user.first_name)
+
+
+
+
+@bot.message_handler(commands=["play"])
+def game_on(message):
+    if not game:
+        bot.send_message(
+            message.chat.id, text='Если хотите играть напишите "готов играть" в лс')
+
+
 
 
 @bot.message_handler(commands=["game"])
@@ -90,32 +79,16 @@ def game_start(message):
         players_roles = db.get_players_roles()
         mafia_usernames = db.get_mafia_usernames()
         for player_id, role in players_roles:
-            if player_id > 5:
-                bot.send_message(player_id, text=role)
-                if role == 'mafia':
-                    bot.send_message(player_id,
-                                     text=f'Все члены мафии:\n{mafia_usernames}')
+            bot.send_message(player_id, text=role)
+            if role == 'mafia':
+                bot.send_message(player_id,
+                                 text=f'Все члены мафии:\n{mafia_usernames}')
         game = True
         bot.send_message(message.chat.id, text='Игра началась!')
-        db.clear(dead=True)
-        game_loop(message)
         return
-    bot.send_message(
-        message.chat.id, text='Недостаточно людей! Добавляю ботов')
-    for i in range(5-players):
-        bot_name = f'robot{i}'
-        db.insert_player(i, bot_name)
-        bot.send_message(message.chat.id, text=f'{bot_name} добавлен!')
-        sleep(0.2)
-    game_start(message)
+    bot.send_message(message.chat.id, text='недостаточно людей!')
 
 
-@bot.message_handler(commands=["play"])
-def game_on(message):
-    if not game:
-        bot.send_message(
-            message.chat.id, text='Если хотите играть напишите "готов играть" в лс')
-        return
 
 
 @bot.message_handler(commands=["kill"])
@@ -136,6 +109,8 @@ def kill(message):
     bot.send_message(message.chat.id, 'Сейчас нельзя убивать')
 
 
+
+
 @bot.message_handler(commands=["kick"])
 def kick(message):
     username = ' '.join(message.text.split(' ')[1:])
@@ -149,10 +124,12 @@ def kick(message):
             bot.send_message(message.chat.id, 'Ваш голос учитан')
             return
         bot.send_message(
-            message.chat.id, 'У вас больше нет права голосовавать')
+            message.chat.id, 'У вас больше нет права голосовать')
         return
     bot.send_message(
         message.chat.id, 'Сейчас ночь вы не можете никого выгнать')
+
+
 
 
 if __name__ == "__main__":
